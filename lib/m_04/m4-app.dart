79b22ec807776e6846d4dfbc2c076ca620f2cc23
Pharallaxe <103119043+Pharallaxe@users.noreply.github.com>
2024-07-31 10:10:53 +0200

    Démo image.

--- lib/m_04/m4-app.dart
+++ lib/m_04/m4-app.dart
@@ -48,7 +48,8 @@ class MyHomePage extends StatelessWidget {
           Row(children: [
             Expanded(child: Text("is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with ")),
             Expanded(child: Text("is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with ")),
-          ],)
+          ],),
+          Image.asset("image_demo.jpg", width: 150)
         ],
       )
     );
